f5f5d49b9a55c2191996a5ab5488498ba40f0113
Captain-S0L0 <obsidian9001@gmail.com>
2024-08-21 22:38:45 -0400

    someone found out, so let the fun begin >:)  - added "unsaveable" preset - works from 24w09a to 24w33a    + savestates players instantly upon signing    + savestates chunks if dropped into a hopper (make sure chunk is loaded by chunk loader / spawn chunks / etc so it doesn't instantly fail to save) - the server will crash when it tries to save the chunk  - added drop option to automatically drop written books straight down upon completing editing

--- src/main/java/com/terriblefriends/booktrolling/mixins/BookEditScreenMixin.java
+++ src/main/java/com/terriblefriends/booktrolling/mixins/BookEditScreenMixin.java
@@ -6,8 +6,12 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.BookEditScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.util.SelectionManager;
+import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -39,7 +43,9 @@ public abstract class BookEditScreenMixin extends Screen {
     @Unique
     private static boolean autoSign = false;
     @Unique
-    private static boolean randomizeChars = true;
+    private static boolean drop = false;
+    @Unique
+    private static boolean randomizeChars = false;
 
     @Unique
     private static final Random RANDOM = new Random();
@@ -57,7 +63,7 @@ public abstract class BookEditScreenMixin extends Screen {
     }
 
     @Unique
-    private void sign(int pageCount, boolean signing, Callable<String> pageGenerator) {
+    private void sign(int pageCount, boolean signing, boolean drop, Callable<String> pageGenerator, Callable<String> titleGenerator) {
         try {
             this.pages.clear();
 
@@ -68,10 +74,17 @@ public abstract class BookEditScreenMixin extends Screen {
             this.dirty = true;
 
             if (signing) {
-                this.title = BRANDING;
+                this.title = titleGenerator.call();
             }
 
             this.finalizeBook(signing);
+
+            if (drop) {
+                float[] rotation = {this.client.player.getYaw(), this.client.player.getPitch()};
+                this.client.getNetworkHandler().sendPacket(new PlayerMoveC2SPacket.LookAndOnGround(0.0f, 0.0f, false));
+                this.client.getNetworkHandler().sendPacket(new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.DROP_ITEM, BlockPos.ORIGIN, Direction.DOWN));
+                this.client.getNetworkHandler().sendPacket(new PlayerMoveC2SPacket.LookAndOnGround(rotation[0], rotation[1], false));
+            }
 
             this.client.setScreen(null);
         }
@@ -85,53 +98,57 @@ public abstract class BookEditScreenMixin extends Screen {
     private void booktrolling$addGuiButtons(CallbackInfo ci) {
         int y = 0;
         this.addDrawableChild(ButtonWidget.builder(Text.literal("1023"), (button) -> {
-            this.sign(100, autoSign, () -> {
+            this.sign(100, autoSign, drop, () -> {
                 StringBuilder builder = new StringBuilder();
                 Callable<Character> charProvider = getCharProvider();
                 for (int i = 0; i < 1023; i++) {
                     builder.append(charProvider.call());
                 }
                 return builder.toString();
-            });
+            }, () -> BRANDING);
         }).dimensions(0, y, 98, 20).build());
         y+=20;
         this.addDrawableChild(ButtonWidget.builder(Text.literal("max unsigned"), (button) -> {
-            this.sign(100, autoSign, () -> {
+            this.sign(100, autoSign, drop, () -> {
                 StringBuilder builder = new StringBuilder();
                 Callable<Character> charProvider = getCharProvider();
                 for (int i = 0; i < 1024; i++) {
                     builder.append(charProvider.call());
                 }
                 return builder.toString();
-            });
+            }, () -> BRANDING);
         }).dimensions(0, y, 98, 20).build());
         y+=20;
         this.addDrawableChild(ButtonWidget.builder(Text.literal("max signed"), (button) -> {
-            this.sign(100, true, () -> {
+            this.sign(100, true, drop, () -> {
                 StringBuilder builder = new StringBuilder();
                 Callable<Character> charProvider = getCharProvider();
                 for (int i = 0; i < 8192; i++) {
                     builder.append(charProvider.call());
                 }
                 return builder.toString();
-            });
+            }, () -> BRANDING);
         }).dimensions(0, y, 98, 20).build());
         y+=20;
         this.addDrawableChild(ButtonWidget.builder(Text.literal("paper"), (button) -> {
-            this.sign(100, autoSign, () -> {
+            this.sign(100, autoSign, drop, () -> {
                 StringBuilder builder = new StringBuilder();
                 Callable<Character> charProvider = getCharProvider();
                 for (int i = 0; i < 320; i++) {
                     builder.append(charProvider.call());
                 }
                 return builder.toString();
-            });
+            }, () -> BRANDING);
+        }).dimensions(0, y, 98, 20).build());
+        y+=20;
+        this.addDrawableChild(ButtonWidget.builder(Text.literal("unsaveable"), (button) -> {
+            this.sign(1, true, drop, () -> "", () -> "123456789012345678901234567890123");
         }).dimensions(0, y, 98, 20).build());
         y+=20;
         this.addDrawableChild(ButtonWidget.builder(Text.literal("clear"), (button) -> {
-            this.sign(1, false, () -> {
+            this.sign(1, false, drop, () -> {
                 return "";
-            });
+            }, () -> BRANDING);
 
         }).dimensions(0, y, 98, 20).build());
         y+=20;
@@ -142,6 +159,9 @@ public abstract class BookEditScreenMixin extends Screen {
         this.addDrawableChild(new ToggleButton(0, this.height-40, 98, 20, Text.literal("RandomizeChars"), () -> {
             randomizeChars = !randomizeChars;
         }, randomizeChars));
+        this.addDrawableChild(new ToggleButton(0, this.height-60, 98, 20, Text.literal("Drop"), () -> {
+            drop = !drop;
+        }, drop));
     }
 
     @Inject(at=@At("HEAD"),method="keyPressedSignMode",cancellable = true)
