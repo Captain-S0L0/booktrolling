8f97b9130e902dd12c22bd5f9f79e5785f180eea
EpikZsoltHUN <75801564+EpikZsoltHUN@users.noreply.github.com>
2025-04-12 12:05:29 +0200

    Update 1.21.5

--- src/main/java/com/terriblefriends/booktrolling/Booktrolling.java
+++ src/main/java/com/terriblefriends/booktrolling/Booktrolling.java
@@ -1,13 +1,18 @@
 package com.terriblefriends.booktrolling;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 
 public class Booktrolling implements ModInitializer {
     public static boolean itemSizeDebug = false;
     public static boolean rawSizes = false;
+    public static ItemSizeDisplay sizeDisplay;
 
     @Override
     public void onInitialize() {
-        //perhaps something will be here later?
+        sizeDisplay = new ItemSizeDisplay();
+        ItemTooltipCallback.EVENT.register((itemStack, tooltipContext, tooltipType, list) -> {
+            sizeDisplay.handleItemSizeDebug(itemStack, tooltipContext, tooltipType, list);
+        });
     }
 }
