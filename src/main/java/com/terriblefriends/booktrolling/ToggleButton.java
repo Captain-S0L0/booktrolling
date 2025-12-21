fb7359f6d18c7766038c11aa0243c470db98f9b8
goodbee <good.bee12345@gmail.com>
2025-12-21 14:28:03 +0100

    feat: update to 1.21.11

--- src/main/java/com/terriblefriends/booktrolling/ToggleButton.java
+++ src/main/java/com/terriblefriends/booktrolling/ToggleButton.java
@@ -1,17 +1,24 @@
 package com.terriblefriends.booktrolling;
 
 import net.minecraft.client.gui.Click;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 
 public class ToggleButton extends ButtonWidget {
-    public ToggleButton(int x, int y, int width, int height, Text message, PressAction onPress, boolean active) {
+    public ToggleButton(int x, int y, int width, int height, net.minecraft.text.Text message, PressAction onPress, boolean active) {
         super(x, y, width, height, message, onPress, ButtonWidget.DEFAULT_NARRATION_SUPPLIER);
         this.active = active;
     }
 
     public boolean isInteractable() {
         return this.visible;
+    }
+
+    @Override
+    protected void drawIcon(DrawContext context, int mouseX, int mouseY, float deltaTicks) {
+        this.drawButton(context);
+        this.drawLabel(context.getHoverListener(this, DrawContext.HoverType.NONE));
     }
 
     public void onClick(Click click, boolean doubled) {
