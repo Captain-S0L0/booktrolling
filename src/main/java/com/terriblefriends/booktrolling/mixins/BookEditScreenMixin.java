7ba18f4b26c4e07d1dcf78084ba5f548c9813a07
Captain-S0L0 <obsidian9001@gmail.com>
2026-06-26 19:18:46 -0400

    update 26.2

--- src/main/java/com/terriblefriends/booktrolling/mixins/BookEditScreenMixin.java
+++ src/main/java/com/terriblefriends/booktrolling/mixins/BookEditScreenMixin.java
@@ -148,7 +148,7 @@ public abstract class BookEditScreenMixin extends Screen {
             if (drop || signing) {
                 int i = this.hand == InteractionHand.MAIN_HAND ? this.owner.getInventory().getSelectedSlot() : 40;
                 this.minecraft.getConnection().send(new ServerboundEditBookPacket(i, this.pages, title));
-                this.minecraft.setScreen(null);
+                this.minecraft.gui.setScreen(null);
             }
 
             if (drop) {
@@ -157,7 +157,7 @@ public abstract class BookEditScreenMixin extends Screen {
         }
         catch (Exception e) {
             LOGGER.error("BookTrolling failed to generate book!", e);
-            this.minecraft.gui.getChat().addClientSystemMessage(Component.literal("<BookTrolling> Error generating book! See logs!").withStyle(ChatFormatting.DARK_RED));
+            this.minecraft.gui.hud.getChat().addClientSystemMessage(Component.literal("<BookTrolling> Error generating book! See logs!").withStyle(ChatFormatting.DARK_RED));
         }
     }
 
--- src/main/java/com/terriblefriends/booktrolling/mixins/LecternScreenMixin.java
+++ src/main/java/com/terriblefriends/booktrolling/mixins/LecternScreenMixin.java
@@ -20,17 +20,17 @@ public abstract class LecternScreenMixin extends Screen {
     @Inject(method="init", at=@At("HEAD"))
     private void booktrolling$addGuiButtons(CallbackInfo ci) {
         this.addRenderableWidget(Button.builder(Component.literal("Shadow Book"), (button) -> {
-            if (this.minecraft == null || this.minecraft.player == null || this.minecraft.gameMode == null) {
+            if (this.minecraft.player == null || this.minecraft.gameMode == null) {
                 return;
             }
 
             int slotToFill = this.minecraft.player.getInventory().getFreeSlot();
             if (slotToFill != -1) {
                 this.minecraft.gameMode.handleContainerInput(this.minecraft.player.containerMenu.containerId, 0, slotToFill, ContainerInput.SWAP, this.minecraft.player);
-                this.minecraft.gui.getChat().addClientSystemMessage(Component.literal("<BookTrolling> Shadowed book...").withStyle(ChatFormatting.RED));
+                this.minecraft.gui.hud.getChat().addClientSystemMessage(Component.literal("<BookTrolling> Shadowed book.").withStyle(ChatFormatting.RED));
             }
             else {
-                this.minecraft.gui.getChat().addClientSystemMessage(Component.literal("<BookTrolling> Error! No room in inventory!").withStyle(ChatFormatting.RED));
+                this.minecraft.gui.hud.getChat().addClientSystemMessage(Component.literal("<BookTrolling> Error! No room in inventory!").withStyle(ChatFormatting.RED));
             }
         }).bounds(this.width - 98, this.height - 40, 98, 20).build());
     }
